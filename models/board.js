c10c83f92b091deb132a1bb54264dfb6a55f2cf2
qixiaofeng-tsmount <qixiaofeng@tsmount.com>
2016-09-19 09:12:22 +0800

    Preparing board.

---
+++ models/board.js
@@ -0,0 +1,5 @@
+function Board(){
+
+};
+
+exports.board = Board;
---
+++ models/cross.js
@@ -0,0 +1,13 @@
+CrossTenant = {
+    Empty:1,
+    Red:2,
+    Green:3,
+    Border:4
+};
+
+function Cross(){
+    this.tenant = CrossTenant.Empty;
+};
+
+exports.cross = Cross;
+exports.cross_tenant = CrossTenant;
