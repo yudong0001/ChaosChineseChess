58a539769c13c3c622c94eecc798b561bdb59cc6
qixiaofeng-tsmount <qixiaofeng@tsmount.com>
2016-10-07 13:31:14 +0800

    Preparing to move the logic to server side.

--- routes/play.js
+++ routes/play.js
@@ -5,12 +5,28 @@ router.all('/', function(req, res){
     if (req.session.count == undefined) {
         req.session.count = 0;
     }
+    console.log('------', req.body.hw);
     req.session.count++;
     res.json({
         result:'success',
         movement:true,
         testValue: req.session.id,
         testCount: req.session.count
+    });
+});
+
+router.post('/flip', function(req, res){
+    req.session.count++;
+    if (req.session.board == undefined) {
+        req.session.board = {
+            placeholder: '占位内容'
+        }
+    }
+    var board = req.session.board;
+    res.json({
+        result:'success',
+        testCount: req.session.count,
+        ph: req.session.board
     });
 });
 
