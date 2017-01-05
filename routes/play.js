a37ceb6617fc0bfbc40c14e38cbf364e1dc1aa61
Dingwei <wei_ding01@126.com>
2017-01-05 17:20:14 +0800

    all logic between Human&AI is ok.

--- routes/play.js
+++ routes/play.js
@@ -3,15 +3,15 @@ var camper = require('../models/camper');
 
 	var EMPTY_NAME = '空';
 
-	function getCellFromMami(jqMami) {
+	/*function getCellFromMami(jqMami) {
         var x = jqMami.css('left');
         var y = jqMami.css('top');
         x = x.substring(0, x.length - 2) / cellSize;
         y = y.substring(0, y.length - 2) / cellSize;
         return {x: x, y: y};
-    }
+    }*/
 
-    function updateMamiForCell(jqMami, cell) {
+    /*function updateMamiForCell(jqMami, cell) {
         if (cell.hidden) {
             return;
         } else {
@@ -22,61 +22,147 @@ var camper = require('../models/camper');
         } else {
             jqMami.removeClass('red black').addClass(cell.camp).text(cell.name);
         }
+    }*/
+
+    function randomInt(max){
+        var rvalue = Math.floor( Math.random()*max );
+        console.log('randomInt value:'+ rvalue);//*******
+        return rvalue;
     }
 
     // AIPlayer 的难度依据 AI 是否知晓盘面全局（未翻开的棋子）划分。
     var aiPlayer = {
         camp: undefined,
         testCouldMove: function(pieceMami, targetMami) {
-            var piece = getCellFromMami(pieceMami);
+            /*var piece = getCellFromMami(pieceMami);
             var target = getCellFromMami(targetMami);
-            return piece.canMoveTo(targetMami);
+            return piece.canMoveTo(targetMami);*/
+            return piece.canMoveTo(target);
         },
-        play: function() {
+        play: function(e) {
             // 电脑检测有无已翻开的己方棋子并尝试走动。
-            var candidates = $('.' + this.camp + '.discovered:not(.empty)').get();
-            var targets = $('.discovered:not(.' + this.camp + ')').get();
+            /*var candidates = $('.' + this.camp + '.discovered:not(.empty)').get();
+            var targets = $('.discovered:not(.' + this.camp + ')').get();*/
+
+            var requ = e.req;
+            var resp = e.res;
+            //var board = requ.session.board.grids;
+            var board = global.board;
+            var rangeX = requ.session.board.columnsCount;
+            var rangeY = requ.session.board.rowsCount-1;
+            var candidates = [];
+            var targets = [];
             var validMovements = [];
+
+            console.log('entr AIPlayer.play.');
+            //console.log('entr AIPlayer.play ,board[1][1] is: %o' ,board[1][1]);//***********
+
+            var count1=count2=0;
+            for (var i = rangeY - 1; i >= 0; i--) {//row number
+                for(var j=0;j<rangeX;j++){//column number
+                    //console.log('for AI candi&target ,current cell discoverd:%o',board[i][j]);//*******
+                    if(!board[i][j].hidden && board[i][j].name!=EMPTY_NAME ){
+                        if(board[i][j].camp==this.camp){
+                            candidates.push(board[i][j]);
+                        }else{
+                            targets.push(board[i][j]);
+                        }
+                    }
+                }
+            };
+
+            //console.log('AIPlayer.candidates is: %o',candidates);//*************
+            //console.log('AIPlayer.targets is: %o',targets);//************
+
             for (var i = 0; i < candidates.length; i++) {
-                var candMami = $(candidates[i]);
-                var candCell = getCellFromMami(candMami);
+                /*var candMami = $(candidates[i]);
+                var candCell = getCellFromMami(candMami);*/
+                var candCell = candidates[i];
+
                 for (var j = 0; j < targets.length; j++) {
-                    var tarMami = $(targets[j]);
-                    var tarCell = getCellFromMami(tarMami);
+                    /*var tarMami = $(targets[j]);
+                    var tarCell = getCellFromMami(tarMami);*/
+                    var tarCell = targets[j];
+
                     if (candCell.canMoveTo(tarCell)) {
-                        validMovements.push({pieceMami:candMami, targetMami:tarMami});
+                        validMovements.push({pieceMami:candCell, targetMami:tarCell});
                     }
                 }
             }
             if (validMovements.length > 0) {
-                var mm = validMovements[0];
-                var piece = getCellFromMami(mm.pieceMami);
-                console.log('电脑的' + piece.name + '走了一步');
-                var target = getCellFromMami(mm.targetMami);
+                /*var mm = validMovements[0];
+                var piece = getCellFromMami(mm.pieceMami);*/
+                var piece = validMovements[0].pieceMami;
+                /*var target = getCellFromMami(mm.targetMami);*/
+                var target = validMovements[0].targetMami;
+                console.log('电脑的' + this.camp+piece.name + ' KO ' +target.camp+target.name);
                 piece.moveTo(target);
-                updateMamiForCell(mm.pieceMami, piece);
-                updateMamiForCell(mm.targetMami, target);
+                resp.json({
+                    action:'kill',
+                    killer:piece,
+                    killed:target
+                });
+                /*updateMamiForCell(mm.pieceMami, piece);
+                updateMamiForCell(mm.targetMami, target);*/
                 return;
             }
 
             // 走动失败，随机（或有选择的，依据难度不同）翻开棋子。
-            var undiscovered = $('.cell:not(.discovered)').get();
+            /*var undiscovered = $('.cell:not(.discovered)').get();*/
+            var undiscovered = [];
+            console.log('start AIPlayer.undiscovered.');//************
+
+            count1=count2=0;
+            for (var i = rangeY - 1; i >= 0; i--) {
+                for(var j=0;j<rangeX;j++){
+                    //console.log('for AI undiscovered ,current cell is: %o',board[i][j]);//********
+                    if(board[i][j].hidden&&board[i][j].name!=EMPTY_NAME){
+                        /*undiscovered[count1++].push(board[i][j]);*/
+                        undiscovered.push(board[i][j]);
+                    }
+                }
+            }
+            console.log('ai.undiscovered length: %o',undiscovered.length);//***********
+
             if (undiscovered.length > 0) {
-                var toBeDiscoveredMami = $(undiscovered[randomInt(undiscovered.length)]);
-                var toBeDiscovered = getCellFromMami(toBeDiscoveredMami);
+                console.log('start to discover a cell by AI.');//**********
+                /*var toBeDiscoveredMami = $(undiscovered[randomInt(undiscovered.length)]);
+                var toBeDiscovered = getCellFromMami(toBeDiscoveredMami);*/
+                var toBeDiscovered = undiscovered[randomInt(undiscovered.length)];
                 console.log('电脑翻开了 ' + toBeDiscovered.camp + ' ' + toBeDiscovered.name);
                 toBeDiscovered.hidden = false;
-                updateMamiForCell(toBeDiscoveredMami, toBeDiscovered);
+                /*updateMamiForCell(toBeDiscoveredMami, toBeDiscovered);*/
+                resp.json({
+                    action:'discover',
+                    cell:toBeDiscovered
+                });
                 return;
             }
 
             // 无可用下一步，则检测是否输局或 Pass 一步。
-            var restPieces = $('.discovered.' + this.camp).get();
-            var restEnemies = $('.discovered.' + camper.getEnemy(this.camp));
+            /*var restPieces = $('.discovered.' + this.camp).get();
+            var restEnemies = $('.discovered.' + camper.getEnemy(this.camp));*/
+            var restPieces = [];
+            var restEnemies = camper.getEnemy(this.camp);
+            count1=count2=0;
+            for (var i = rangeY - 1; i >= 0; i--) {
+                for(var j=0;j<rangeX;j++){
+                    console.log('for AI restPieces,current cell is %o',board[i][j]);//********
+                    if(board[i][j].camp == this.camp){
+                        restPieces[count1++].push(board[i][j]);
+                    }
+                }
+            };
             if (restPieces.length == 0 && restEnemies.length > 0) {
                 console.log('电脑认输');
+                resp.json({
+                    action:'finished',
+                });
             } else {
                 console.log('电脑 Pass 一步');
+                resp.json({
+                    action:'pass',
+                });
             }
         },
     };//***AI Player***
@@ -107,17 +193,20 @@ var camper = require('../models/camper');
                 updateMamiForCell(this.selectedAlly, selected);
                 updateMamiForCell(targetMami, targetCell);
                 this.selectedAlly = undefined;*/
+                selected.moveTo(targetCell);
+                this.selectedAlly = undefined;
+                console.log('人类' + this.camp+killer.name + ' KO ' +killed.camp+killed.name);
                 resp.json({
                     action:'kill',
                     killer:killer,
                     killed:killed
                 });
-                selected.moveTo(targetCell);
-                this.selectedAlly = undefined;
-                //aiPlayer.play();
+                //aiPlayer.play(requ,resp);
             } else {
                 console.log('不能这么走，你要去学一学中国象棋基本规则。');
-                resp.json();
+                resp.json({
+                    action:'canNotMove'
+                });
             }
         },
         play: function(e) {
@@ -130,7 +219,7 @@ var camper = require('../models/camper');
             var cellpos = {x:e.posx ,y:e.posy};
             var cell = global.board[cellpos.y][cellpos.x];
 
-            console.log('get board from global:%o', cell);//***************
+            console.log('get req cell from global board: %o',cell);//***************
             console.log('who is this in humanPlayer.play: %o',this);//**************
 
 
@@ -187,14 +276,16 @@ var camper = require('../models/camper');
                             console.log('取消选择。');
                             this.selectedAlly = undefined;
                             resp.json({
-                                action:'unselect'
+                                action:'unselect',
+                                cell:cell
                             });
                         } else {
                             console.log('切换选择。');
                             //clicked.addClass('selected');
                             this.selectedAlly = cell;
                             resp.json({
-                                action:'exchange'
+                                action:'xselect',
+                                cell:cell
                             });
                         }
                     } else {
@@ -203,12 +294,16 @@ var camper = require('../models/camper');
                     }
                 }
             }
-        },
+        }
     };//***humanPlayer***
 
 router.post('/',function(req,res){
-	console.log('-----:: %o',req.body.x);//************
-    humanPlayer.play({req:req ,res:res ,posx:req.body.x ,posy:req.body.y});
+	console.log('-----:'+req.body.player +':'+req.body.x);//************
+    if(req.body.player=="humanPlayer"){
+        humanPlayer.play({ req:req ,res:res ,posx:req.body.x ,posy:req.body.y });
+    }else{
+        aiPlayer.play({req:req ,res:res});
+    }
 });
 
 /*router.all('/', function(req, res){
